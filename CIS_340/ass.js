2fd1e5e84549788a4ae897bf4eae5b78940d0d10
ksaisujith2810 <64578039+ksaisujith2810@users.noreply.github.com>
2022-01-29 16:11:11 +0530

    Add files via upload

---
+++ CIS_340/ass.js
@@ -0,0 +1,13 @@
+// Declared & Assigned
+
+// string concatenation
+const year = 2022;
+const myNameTwo = "Hello Spring";
+// Here we need the output has Hello Spring 2022
+const outPut = myNameTwo + ' ' + year;
+console.log(outPut);
+
+// Here it is the feature of ES6
+const mySentence = `Hello Spring ${year}`;
+console.log(mySentence);
+
